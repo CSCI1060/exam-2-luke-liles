80e099a55fb823978b57508eabf0eeb36465b24e
luke-liles <luke.liles@slu.edu>
2024-11-10 19:57:38 -0600

    simpleBlackjack.m

--- simpleBlackjack.m
+++ simpleBlackjack.m
@@ -1,15 +1,38 @@
 function avgScore = simpleBlackjack(limit, trials)
-%
-% This function simulates playing many hands of
-% Simple Blackjack where the player continually
-% hits up to a given limit. Final score for each
-% hand is computed, and the average score over all
-% hands is returned.
-%
-% INPUT limit – The player always hits if their
-% hand value is less than limit
-% INPUT trials – The number of hands to simulate
-%
-% OUTPUT avgScore – The average final score of
-% all hands.
+% This function simulates playing Simplified Blackjack
+% INPUT: 
+%   limit - The player always hits if their hand value is less than limit
+%   trials - The number of hands to simulate
+% OUTPUT:
+%   avgScore - The average final score of all hands
+
+% Possible card values 
+cards = [2 3 4 5 6 7 8 9 10 10 10 10];
+
+ % Initial total score over all trials
+ totalScore = 0;
+
+ % Simulate "trials" hands
+ for t = 1:trials
+      handTotal = 0;  % Start each hand with a total of 0
+
+      % Simulate a single hand
+       while handTotal < limit
+           % Draw a card by randomly selecting from "cards"
+            card = cards(randi(length(cards)));
+            handTotal = handTotal + card;
+
+            % Stop drawing if the total exceeds 21
+            if handTotal > 21
+                break;
+            end
+       end
+
+        % Add the final hand total to the total score
+        totalScore = totalScore + handTotal;
+ end
+  % Compute the average score
+   avgScore = totalScore / trials;
+end
+
 
